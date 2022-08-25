de967c7b5d61725e84f42b04db9d04e9a5f879b0
Markus Schlichting <markus.schlichting@karakun.com>
2022-08-25 15:29:16 +0200

    Sample for Camera and SHow Image

---
+++ README.md
@@ -0,0 +1,4 @@
+# NativeScript Camera Photo Display Sample
+
+Bastic sample app with NativeScript 8.3 that takes a photo and directly shows it.
+
--- src/app/item/items.component.ts
+++ src/app/item/items.component.ts
@@ -1,18 +1,37 @@
 import { Component, OnInit } from '@angular/core'
+import { requestPermissions } from '@nativescript/camera';
+import * as camera from "@nativescript/camera";
+import { ImageSource } from "@nativescript/core";
 
-import { Item } from './item'
-import { ItemService } from './item.service'
 
 @Component({
   selector: 'ns-items',
   templateUrl: './items.component.html',
 })
 export class ItemsComponent implements OnInit {
-  items: Array<Item>
+  image: ImageSource;
 
-  constructor(private itemService: ItemService) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.items = this.itemService.getItems()
-  }
+    var self = this;
+  
+      requestPermissions().then(
+        function success() {
+          camera.takePicture()
+            .then((imageAsset) => {
+              console.log("Result is an image asset instance");
+              ImageSource.fromAsset(imageAsset).then((source) => {
+                self.image = source;
+              });    
+            }).catch((err) => {
+              console.log("Error -> " + err.message);
+            });
+        },
+        function failure() {
+          // permission request rejected
+          // ... tell the user ...
+        }
+      );
+    }
 }
